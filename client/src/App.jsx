ff6841df05d1e160efa69ed41329050c03fb56d6
loveCiForever <quanghuy71847@gmail.com>
2025-02-09 02:22:37 +0700

    First commit: Setup project FE: react+vite+tailwindcss, BE: express+mongodb

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const App = () => {
+  return (
+    <div className="flex w-full h-screen justify-center items-center bg-red-200">
+      <h1 className="flex w-full h-full justify-center items-center text-8xl font-bold">
+        Hi, we are ATH group
+      </h1>
+    </div>
+  );
+};
+
+export default App;
