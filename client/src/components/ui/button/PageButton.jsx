15e49a47893af94ac46e6b115e63a873c1ea1d0a
Quang Huy (Victor) Nguyen <107240800+loveCiForever@users.noreply.github.com>
2025-04-01 16:28:32 +0700

    Update PageButton.jsx

--- client/src/components/ui/button/PageButton.jsx
+++ client/src/components/ui/button/PageButton.jsx
@@ -1,13 +1,16 @@
 import { useNavigate } from "react-router-dom";
-
-const PageButton = ({ currentLocationPath, navigateTo, name, solid }) => {
+import { getBasePath } from "../../utils/PathSplitment";
+const PageButton = ({ currentBasePath, navigateTo, name, solid }) => {
   const navigate = useNavigate();
+
+  // console.log(currentLocationPath);
+  // console.log(navigateTo);
 
   return (
     <div className="relative">
       <button
         className={`page-button text-lg font-bold hover:text-orange-500 active:scale-[.90] active:duration-90 transition-all ${
-          currentLocationPath == navigate ? "text-orange-500 " : ""
+          currentBasePath == getBasePath(navigateTo) ? "text-orange-500 " : ""
         } ${
           solid == true
             ? "py-2 px-6 bg-black/80 rounded-xl  text-white font-medium text-sm tracking-wider hover:text-white hover:bg-black/50"
@@ -19,7 +22,7 @@ const PageButton = ({ currentLocationPath, navigateTo, name, solid }) => {
       >
         {name}
       </button>
-      {currentLocationPath == navigateTo && solid == false && (
+      {currentBasePath == getBasePath(navigateTo) && solid == false && (
         <hr className="absolute left-0 right-0 border-0 bottom-[-5px] h-[3px] bg-orange-400" />
       )}
     </div>
