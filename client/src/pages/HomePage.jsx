6684e512c2ae6c932d3564735d4376b3a1a9301b
Quang Huy (Victor) Nguyen <107240800+loveCiForever@users.noreply.github.com>
2025-04-01 16:30:49 +0700

    Update HomePage.jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,22 @@
 import { useContext, useEffect, useState } from "react";
 import NavBar from "../components/layout/navbar/NavBar";
-
-const HomePage = ({ theme }) => {
+import { ThemeContext } from "../App";
+import { UserContext } from "../App";
+const HomePage = () => {
   useEffect(() => {
     document.title = "Home Page";
   });
+
+  const { theme } = useContext(ThemeContext);
+  const { userAuth } = useContext(UserContext);
+
+  useEffect(() => {
+    console.log(theme);
+  }, [theme]);
+
+  // useEffect(() => {
+  //   console.log(userAuth);
+  // });
 
   return (
     <div
