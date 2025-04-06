049c2b11b03617318a4a4ba4000d76b59a339b9d
Quang Huy (Victor) Nguyen <107240800+loveCiForever@users.noreply.github.com>
2025-04-06 22:45:57 +0700

    Delete server directory

--- server/src/controllers/helloworld.controller.js
+++
@@ -1,9 +0,0 @@
-// ./server/src/controller/helloworld.controller.js
-
-const helloworld = async (req, res) => {
-  const helloworld = "Hello World !";
-  // return res.status(200).json({ message: helloworld });
-  return res.formatter.ok(helloworld);
-};
-
-export { helloworld };
