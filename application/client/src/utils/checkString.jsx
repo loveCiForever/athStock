6b780bba55fae105e473234fe846cfcf2f738776
loveCiForever <quanghuy71847@gmail.com>
2025-06-05 00:16:21 +0700

    remove uneccessary file, add unit test for formatNumber, formatString

--- application/client/src/utils/checkString.jsx
+++
@@ -1,7 +0,0 @@
-// application/client/src/utils/checkString.jsx
-
-export const checkString = (string) => {
-  if (!string || typeof string !== "string") {
-    return "Invalid input: Please provide a valid string";
-  }
-};
